f530c210d10bd2fb3fb58b8ae75d8e60958799e6
asumagang <janaidrhean@gmail.com>
2019-12-13 17:09:30 +0800

    2222222222

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReportsComponent } from './reports/reports.component';
-
+import { ChartsModule } from 'ng2-charts';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +18,10 @@ import { ReportsComponent } from './reports/reports.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ChartsModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label, Color } from 'ng2-charts';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
 
+  public barChartData: ChartDataSets[] = [
+    { data: [65000, 59000, 80000, 8100, 56000, 55000, 40000], label: 'Nnumber of Beneficiaries' },
+    { data: [28000, 48000, 40000, 19000, 86000, 27000, 90000], label: 'Amount Disbursed' }
+  ];
+
+  public barChartColors: Color[] = [
+    { backgroundColor: '#0D9FF7 ' },
+    { backgroundColor: '#4DEF25 ' },
+  ]
   constructor() { }
 
   ngOnInit() {
